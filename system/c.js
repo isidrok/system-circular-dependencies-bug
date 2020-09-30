7f7ec8e61d565c8ae24f7cf8a022b0a5771bc92a
Joel Denning <joeldenning@gmail.com>
2020-09-30 11:40:22 -0600

    Hoisted exports

--- system/c.js
+++ system/c.js
@@ -1,27 +1,27 @@
 System.register(['./b.js'], function (exports) {
 	'use strict';
+	exports('a', createCommonjsModule);
+
+	function createCommonjsModule(fn, basedir, module) {
+		return module = {
+			path: basedir,
+			exports: {},
+			require: function (path, base) {
+				return commonjsRequire(path, (base === undefined || base === null) ? module.path : base);
+			}
+		}, fn(module, module.exports), module.exports;
+	}
+
+	function commonjsRequire () {
+		throw new Error('Dynamic requires are not currently supported by @rollup/plugin-commonjs');
+	}
+
 	var b;
 	return {
 		setters: [function (module) {
 			b = module.b;
 		}],
 		execute: function () {
-
-			exports('a', createCommonjsModule);
-
-			function createCommonjsModule(fn, basedir, module) {
-				return module = {
-					path: basedir,
-					exports: {},
-					require: function (path, base) {
-						return commonjsRequire(path, (base === undefined || base === null) ? module.path : base);
-					}
-				}, fn(module, module.exports), module.exports;
-			}
-
-			function commonjsRequire () {
-				throw new Error('Dynamic requires are not currently supported by @rollup/plugin-commonjs');
-			}
 
 			var c = exports('c', createCommonjsModule(function (module, exports) {
 			console.log(b);
